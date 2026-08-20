fc0efda4633030df31b1b54ca4ac1f4c5a1cb092
zhangjiyuan22 <117197511+zhangjiyuan22@users.noreply.github.com>
2026-08-20 19:44:34 +0800

    Add files via upload

---
+++ simulate_events.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(1,'pycode/')
+from functions_cpu import *
+# from functions_cpu_parallel import *
+from getconfig import *
+import time
+
+
+if __name__ == "__main__":
+    time_begin = time.time()
+    ###############################
+
+    # eventname = 'ob110950'
+    # eventname = 'ob110950_uniform_mass_prior'
+    # eventname = 'ob110950_test'
+    # eventname = 'ob110950_5e10'
+    eventname = 'ob110950_isolens'
+    # eventname = 'ob110950_5e10based_rerun'
+
+    config = EventInfo()
+    config = getEventInfo(eventname)
+
+    SimulateEvents(config)
+
+    time_end = time.time()
+    print('\nTotal cost: %.2f min'%((time_end-time_begin)/60))
